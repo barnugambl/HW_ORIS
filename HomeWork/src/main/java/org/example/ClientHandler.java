c76e8bc4ed5e79b1f751ecd65eb3dceef3a19482
barnugambl <143866281+barnugambl@users.noreply.github.com>
2025-01-29 19:10:56 +0300

    Add files via upload

---
+++ HomeWork/src/main/java/org/example/ClientHandler.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientHandler implements Runnable{
+
+    public static List<ClientHandler> clientHandlers = new ArrayList<>();
+
+    private Socket socket;
+
+    private BufferedWriter bufferedWriter;
+
+    private BufferedReader bufferedReader;
+
+    private String clientUsername;
+
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER " + clientUsername + " подключился");
+
+        } catch (Exception e) {
+            closeEverything(socket,bufferedReader,bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()){
+            try {
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket,bufferedReader,bufferedWriter);
+                break;
+            }
+        }
+    }
+
+
+    public void broadcastMessage(String messageToSend){
+        for (ClientHandler clientHandlers : clientHandlers){
+            try {
+                if(!clientHandlers.clientUsername.equals(clientUsername)) {
+                    clientHandlers.bufferedWriter.write(messageToSend);
+                    clientHandlers.bufferedWriter.newLine();
+                    clientHandlers.bufferedWriter.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket,bufferedReader,bufferedWriter);
+            }
+        }
+    }
+
+    public void removeClientHandler(){
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUsername + " вышел");
+    }
+
+
+    public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+        try {
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }
+            if(bufferedWriter != null){
+                bufferedWriter.close();
+            }
+            if(socket != null){
+                socket.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
